1114035671cb716934c7218c7d89a6a376d9e9d3
gamzeumac <umacgamze49@gmail.com>
2021-04-19 23:12:31 +0200

    variable

---
+++ js.js
@@ -0,0 +1,23 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+
+const songs = ['hey', 'summer', 'ukulele']
+
+let songIndex = 1
+
+loadSong(songs[songIndex])
+
+function loadSong(song) {
+    title.innerHTML = song,
+    audio.src = `music/${song}.mp3`,
+    cover.src =`image/${sog}.jpg`
+    
+}
